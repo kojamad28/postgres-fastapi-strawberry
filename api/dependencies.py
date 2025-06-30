8b085592d1fa904da502cde0770eebd105dd3c46
kojamad28 <kojamad28@gmail.com>
2025-06-30 17:44:23 +0900

    added dependencies

---
+++ api/dependencies.py
@@ -0,0 +1,36 @@
+from dotenv import dotenv_values
+from sqlalchemy import URL
+from sqlmodel import SQLModel, Session, create_engine
+
+
+#config = dotenv_values("db/.env.dev")
+config = dotenv_values("db/.env")
+
+
+DEBUG = config.get("DEBUG", "false").lower() == "true"
+
+
+db_url = URL.create(
+        "postgresql+psycopg",
+        username=config["POSTGRES_USER"],
+        password=config["POSTGRES_PASSWORD"],  # plain (unescaped) text
+        host=config.get("POSTGRES_HOST", "postgres"),
+        port=config.get("POSTGRES_PORT", "5432"),
+        database=config.get("POSTGRES_DB", "postgres"),
+        query={"options": f"-c search_path={config.get('POSTGRES_SCHEMA', 'public')}"},
+)
+connect_args = {}
+engine = create_engine(db_url, echo=DEBUG, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+async def get_session():
+    with Session(engine) as session:
+        try:
+            yield session
+        except:
+            session.rollback()
+            raise
