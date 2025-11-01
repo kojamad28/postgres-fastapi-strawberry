7f332139d5c672d2aa035895f50ce884754fe653
kojamad28 <kojamad28@gmail.com>
2025-11-02 08:56:42 +0900

    added DEBUG as an environment variable

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,12 +1,22 @@
+import os
 from pathlib import Path
 
 from sqlmodel import SQLModel, Session
 
 from db.engine import get_engine
 
+
+def read_boolean(value: str) -> bool:
+    return value.lower() in ("true", "t", "yes", "y", "on", "1")
+
+DEBUG = read_boolean(str(os.environ.get("DEBUG", "False")))
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-#DOTENV_PATH = BASE_DIR / "db" / ".env.dev"
-DOTENV_PATH = BASE_DIR / "db" / ".env"
+
+if DEBUG:
+    DOTENV_PATH = BASE_DIR / "db" / ".env.dev"
+else:
+    DOTENV_PATH = BASE_DIR / "db" / ".env"
 
 engine = get_engine(DOTENV_PATH)
 
